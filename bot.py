2ab273fc89f3863308a645066c9a0dc89946c01e
minty-developer <gimminseung036@gmail.com>
2026-01-27 14:28:15 +0900

    Update bot.py
    서버 ID를 자동으로 알게 수정

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ class MyClient(discord.Client):
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-        guild = discord.Object(id=1422188398138888293)
+        guild = discord.Object(id={guild.id})
         self.tree.copy_global_to(guild=guild)
         await self.tree.sync(guild=guild)
         print("✅ 길드 슬래시 커맨드 강제 동기화 완료")
